566fa545d2c83deb5af0bb78639f93efcd778914
YASMIN HAILITON SANTANA TEIXEIRA <yasmin.hsteixeira@senacsp.edu.br>
2023-07-28 17:24:54 -0300

    adicionado o Ef e criado os models de Author e publisher

---
+++ biblioteca/Models/Author.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace biblioteca.Models
+{
+	[Table("Authors")]
+	public class Author
+	{
+
+		public int Id { get; set; }
+		[Required]
+		[StringLength(50)]
+
+		//nome da coluna
+		[Column("AuthorName")]
+		public string? Name { get; set; }
+
+	}
+}
---
+++ biblioteca/Models/Publisher.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace biblioteca.Models
+{
+	//nome da tabela
+	[Table("Publishers")]
+	public class Publisher
+	{
+		public int Id { get; set; }
+		[Required]
+		[StringLength(50)]
+
+		//nome da coluna
+		[Column("PublisherName")]
+		public string? Name { get; set; }
+		[MaxLength(80), MinLength(4)]
+		public string? Site { get; set; }
+
+	}
+}
